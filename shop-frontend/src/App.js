995bd1d8d2647fbf79bf5b4ecf4b23fd3a092a6b
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2021-06-13 12:45:18 +0200

    Add header

--- shop-frontend/src/App.js
+++ shop-frontend/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Header from './components/Header/Header';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import UserCheckout from './components/UserCheckout/UserCheckout';
+import OrderHistory from './components/OrderHistory/OrderHistory';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+      <BrowserRouter>
+    <>
+      <Header/>
+  
+      <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/user-checkout" component={UserCheckout} exact />
+        <Route path="/order-history" component={OrderHistory} exact />
+      </Switch>
+      
+   
+       
+
+    </>
+    </BrowserRouter>
+    
   );
 }
 
--- shop-frontend/src/components/Header/Header.js
+++ shop-frontend/src/components/Header/Header.js
@@ -1,6 +1,42 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container=styled.div` 
+background-color:#00a1f1;
+display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10%;
+`;
+const Logo=styled.div` 
+h1{
+    color:white;
+}
+`;
+const Nav=styled.div` 
+a{
+    text-decoration: none;
+    padding: 15px;
+    color: white;
+}
+`;
+
+
 function Header(){
     return(
-        <h1>Header</h1>
+        <Container>
+        <Logo>
+            <h1>React shop</h1>
+        </Logo>
+        <Nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/user-checkout">Checkout</NavLink>
+        <NavLink to="/order-history">Order history</NavLink>
+        </Nav>
+        
+        </Container>
+
     )
 }
 
